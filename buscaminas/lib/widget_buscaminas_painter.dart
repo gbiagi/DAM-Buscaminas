989df577082f0abd2bf1e9a735cdb85964b6d6c4
MatillaSan <118919845+MatillaSan@users.noreply.github.com>
2023-10-25 00:59:17 +0200

    Juego funcional

--- buscaminas/lib/widget_buscaminas_painter.dart
+++ buscaminas/lib/widget_buscaminas_painter.dart
@@ -1,4 +1,3 @@
-import 'dart:math';
 import 'dart:ui' as ui;
 import 'package:flutter/material.dart'; // per a 'CustomPainter'
 import 'app_data.dart';
@@ -85,7 +84,7 @@ class WidgetBuscaminasPainter extends CustomPainter {
     for (int i = 0; i < appData.boardSize; i++) {
       for (int j = 0; j < appData.boardSize; j++) {
         // Comprobar si es una bandera
-        if (appData.board[i][j] == '!') {
+        if ((appData.board[i][j] == '!') | (appData.board[i][j] == '+!')) {
           double x0 = j * cellWidth;
           double y0 = i * cellHeight;
           double x1 = (j + 1) * cellWidth;
@@ -94,7 +93,7 @@ class WidgetBuscaminasPainter extends CustomPainter {
           drawImage(canvas, appData.imageFlag!, x0, y0, x1, y1);
         }
         // Comprobar si es una bomba
-        else if (appData.board[i][j] == '+') {
+        else if ((appData.board[i][j] == '+') & (appData.gameIsOver)) {
           double x0 = j * cellWidth;
           double y0 = i * cellHeight;
           double x1 = (j + 1) * cellWidth;
@@ -103,15 +102,6 @@ class WidgetBuscaminasPainter extends CustomPainter {
           drawImage(canvas, appData.imageBomb!, x0, y0, x1, y1);
         }
         // Comprobar si es una bomba explotada
-        else if (appData.board[i][j] == 'x') {
-          double x0 = j * cellWidth;
-          double y0 = i * cellHeight;
-          double x1 = (j + 1) * cellWidth;
-          double y1 = (i + 1) * cellHeight;
-
-          drawImage(canvas, appData.imageExplosion!, x0, y0, x1, y1);
-        }
-        // Comprobar si es la casilla palomita
         else if (appData.board[i][j] == 'p') {
           double x0 = j * cellWidth;
           double y0 = i * cellHeight;
@@ -120,8 +110,35 @@ class WidgetBuscaminasPainter extends CustomPainter {
 
           drawImage(canvas, appData.imagePopcorn!, x0, y0, x1, y1);
         }
+        // Comprobar si es la casilla palomita
+        else if (appData.board[i][j] == 'x') {
+          double x0 = j * cellWidth;
+          double y0 = i * cellHeight;
+          double x1 = (j + 1) * cellWidth;
+          double y1 = (i + 1) * cellHeight;
+
+          drawImage(canvas, appData.imageExplosion!, x0, y0, x1, y1);
+        }
         // Comprobar si en la casilla hay un numero
-        else {
+        else if ((appData.board[i][j] == '+') | (appData.board[i][j] == '-')) {
+          double x0 = j * cellWidth;
+          double y0 = i * cellHeight;
+          double x1 = (j + 1) * cellWidth;
+          double y1 = (i + 1) * cellHeight;
+          final textPainter = TextPainter(
+            text: const TextSpan(text: "UwU", style: textStyle),
+            textDirection: TextDirection.ltr,
+          );
+
+          textPainter.layout(maxWidth: x1 - x0);
+
+          final position = Offset(
+            x0 + (x1 - x0 - textPainter.width) / 2,
+            y0 + (y1 - y0 - textPainter.height) / 2,
+          );
+
+          textPainter.paint(canvas, position);
+        } else {
           double x0 = j * cellWidth;
           double y0 = i * cellHeight;
           double x1 = (j + 1) * cellWidth;
@@ -145,8 +162,8 @@ class WidgetBuscaminasPainter extends CustomPainter {
   }
 
   // Dibuixa el missatge de joc acabat
-  void drawGameOver(Canvas canvas, Size size) {
-    String message = "El joc ha acabat!";
+  void drawGameOver(Canvas canvas, Size size, String msg) {
+    String message = msg;
 
     const textStyle = TextStyle(
       color: Colors.black,
@@ -181,17 +198,16 @@ class WidgetBuscaminasPainter extends CustomPainter {
     textPainter.paint(canvas, position);
   }
 
-  // TODO cambiar este metedo y el de arriba
-  // Funció principal de dibuix
+  // Metodo para pintar final
   @override
   void paint(Canvas canvas, Size size) {
     drawBoardLines(canvas, size);
     drawBoardStatus(canvas, size);
     if (appData.gameIsOver) {
       if (appData.gameIsWin) {
-        drawGameOver(canvas, size); // Se dibuja final de ganar
+        drawGameOver(canvas, size, "Victoria"); // Se dibuja final de ganar
       } else {
-        drawGameOver(canvas, size); // Se dibuja final de perder
+        drawGameOver(canvas, size, "Derrota"); // Se dibuja final de perder
       }
     }
   }
